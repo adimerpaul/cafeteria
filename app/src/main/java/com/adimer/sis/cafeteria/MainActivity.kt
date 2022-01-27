94a5453b914ee0d38eff84aaf69c8d1a38885371
adimerpaul <adimer101@email.com>
2022-01-26 21:02:31 -0400

    mi primera version

---
+++ app/src/main/java/com/adimer/sis/cafeteria/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.adimer.sis.cafeteria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    val db=Firebase.firestore
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val arrayAdapter:ArrayAdapter<*>
+        val cafes= mutableListOf("CAFE CLASICO","CAFE CON LECHE","CAFE PURO","SUBMARINO")
+        val list=findViewById<ListView>(R.id.list)
+        arrayAdapter= ArrayAdapter(this,android.R.layout.simple_list_item_1,cafes)
+        list.adapter=arrayAdapter
+    }
+}
