f074faaef403497b7371dbad7c9695300e11f81e
adimerpaul <adimer101@email.com>
2022-01-27 20:50:55 -0400

    proyecto cafe terminado para estudiantes

--- app/src/main/java/com/adimer/sis/cafeteria/MainActivity.kt
+++ app/src/main/java/com/adimer/sis/cafeteria/MainActivity.kt
@@ -2,20 +2,41 @@ package com.adimer.sis.cafeteria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-//    val db=Firebase.firestore
+    val db=Firebase.firestore
+    val TAG="MYTAG"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val arrayAdapter:ArrayAdapter<*>
+        var arrayAdapter:ArrayAdapter<*>
         val cafes= mutableListOf("CAFE CLASICO","CAFE CON LECHE","CAFE PURO","SUBMARINO")
         val list=findViewById<ListView>(R.id.list)
-        arrayAdapter= ArrayAdapter(this,android.R.layout.simple_list_item_1,cafes)
-        list.adapter=arrayAdapter
+
+
+
+        db.collection("cafes")
+//            .whereEqualTo("state", "CA")
+            .addSnapshotListener { value, e ->
+                if (e != null) {
+                    Log.e(TAG, "Listen failed.", e)
+                    return@addSnapshotListener
+                }
+
+                val cities = ArrayList<String>()
+                for (doc in value!!) {
+                    doc.getString("nombre")?.let {
+                        cities.add(it)
+                    }
+                }
+//                Log.e(TAG, "Current cites in CA: $cities")
+                arrayAdapter= ArrayAdapter(this,android.R.layout.simple_list_item_1,cities)
+                list.adapter=arrayAdapter
+            }
     }
 }
---
+++ app/src/main/java/com/adimer/sis/cafeteria/MyFirebaseMessagingService.java
@@ -0,0 +1,6 @@
+package com.adimer.sis.cafeteria;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
